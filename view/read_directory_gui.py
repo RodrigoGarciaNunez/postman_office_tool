644624bd358639084f7e713bfc2c48c4b8516b47
RodrigoGarciaNunez <2203025158@cua.uam.mx>
2025-12-15 18:06:10 -0600

    Worked con read_directory_gui

--- view/read_directory_gui.py
+++ view/read_directory_gui.py
@@ -11,10 +11,10 @@ class read_directory_gui(Frame):
 
         #context.screens["directory_gui"] = self
         # Etiqueta y entrada para archivo de directorio
-        Label(self, text="Directorio:", bg="skyblue").grid(row=0, column=0, padx=0, pady=0, sticky="w")
+        Label(self, text="Directorio:", bg="skyblue").grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
         
         entry_file_dir = Entry(self, width=20)
-        entry_file_dir.grid(row=0, column=1, padx=0, pady=0, sticky="w")
+        entry_file_dir.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
         btn_carpeta = Button(self, text="Seleccionar", command= lambda : context.postman_.seleccionar_file(entry_file_dir))
         btn_carpeta.grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
 
@@ -27,26 +27,30 @@ class read_directory_gui(Frame):
 
         # Botón de buscar
         btn_buscar = Button(self, text="Leer", command=lambda : context.postman_.leer_directorio(entry_file_dir,directory_text))
-        btn_buscar.grid(row=2, column=6, pady=10, sticky="nsew")
+        btn_buscar.grid(row=1, column=6, pady=10, sticky="nsew")
 
 
-        #frame_directory = Frame(self, bg="lightgray", height=50)
-        #frame_directory.pack(expand=True)
         
-        ### ocupa tener su propio frame o zona para que se no afecte sus demensiones,mañana queda
-        Label(self, text= "Directorio").grid(row=0, column= 7, padx=5, pady=5, sticky="nsew")
-        directory_text = scrolledtext.ScrolledText(self, width=50, height=40)
-        directory_text.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+        frame_directory = Frame(self, bg="lightgray", height=50)
+        frame_directory.grid(row=2, column=0, columnspan=7)
+        #frame_directory.pack(fill="both")
+        
+        Label(frame_directory, text= "Directorio").grid(row=0, column= 0, padx=5, pady=5)
+        directory_text = scrolledtext.ScrolledText(frame_directory, width=60, height=30)
+        directory_text.grid(row=1, column=0, padx=10, pady=10, sticky="nsew" )
 
         btn_send =  Button(self, text="Enviar", command= lambda : context.postman_.send())
-        btn_send.grid(row = 2, column= 8, padx=10, pady= 10, sticky="nsew")
+        btn_send.grid(row = 3, column= 5, padx=10, pady= 10, sticky="nsew")
+
+        for i in range(0, 8):
+            self.columnconfigure(i, weight=1)
 
         #self.grid_configure()
 
 
 
 if __name__ == "__main__":
-    container = Frame(height="800", width="600")
+    container = Frame(height="600", width="800")
     container.pack(fill="both", expand=True)
     container.pack_propagate(False)
     test = read_directory_gui(container, Tk)
